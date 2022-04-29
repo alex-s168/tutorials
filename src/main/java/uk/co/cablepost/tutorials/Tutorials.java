208a044940eee7756472802318ed8f6f427bd0fd
BillBodkin <billyg270@gmail.com>
2022-04-29 11:08:16 +0100

    Init

---
+++ src/main/java/uk/co/cablepost/tutorials/Tutorials.java
@@ -0,0 +1,10 @@
+package uk.co.cablepost.tutorials;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tutorials implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/uk/co/cablepost/tutorials/client/TutorialsClient.java
@@ -0,0 +1,13 @@
+package uk.co.cablepost.tutorials.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TutorialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
